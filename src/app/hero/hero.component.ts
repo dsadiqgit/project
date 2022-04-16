f510cfad639916e57cb1e84384a3b5a8bc1df119
Dami Sadiq <dsadiq1994@gmail.com>
2022-04-16 13:19:33 +0100

    updating text on hero section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -18,43 +18,50 @@ export class HeroComponent implements OnInit {
 
   @HostListener('window:scroll', []) onWindowScroll() {
     $(window).scroll(function () {
+      var lgScreen = window.matchMedia('(min-width: 700px)');
+
       var scroll = $(window).scrollTop();
       var mac = $('#mac');
       var imageDiv = $('#js-hero');
       var macDiv = $('#mac-div');
+      var mouse = $('#mouse');
 
-      mac.css({
-        width: 100 - scroll / 2 + '%',
-        top: -300 + scroll * 2 + 'px',
-        opacity: 0.01 + scroll / 100,
-      });
-      imageDiv.css({
-        top: 600 + scroll / 2 + 'px',
-      });
-      macDiv.css({
-        top: 0 + scroll / 1.8 + 'px',
-      });
-
-      if (mac.width() <= 400) {
+      if (lgScreen.matches) {
         mac.css({
-          width: 400,
-          top: -57,
-        });
-        macDiv.css({
-          top: 67,
+          width: 100 - scroll / 2 + '%',
+          top: -300 + scroll * 2 + 'px',
+          opacity: 0.01 + scroll / 130,
         });
         imageDiv.css({
-          top: 670,
+          top: 600 + scroll / 2 + 'px',
         });
-      }
+        macDiv.css({
+          top: 0 + scroll / 1.8 + 'px',
+        });
 
-      if (mac.width() <= 519) {
-        macDiv.addClass('active');
+        if (mac.width() <= 400) {
+          mac.css({
+            width: 400,
+            top: -57,
+          });
+          imageDiv.css({
+            top: 670,
+          });
+        }
+
+        if (mac.width() <= 519) {
+          macDiv.addClass('active');
+          mouse.addClass('d-none');
+          macDiv.css({
+            top: 67,
+          });
+        }
+        if (mac.width() >= 520) {
+          macDiv.removeClass('active');
+          mouse.removeClass('d-none');
+        }
       }
-      if (mac.width() >= 520) {
-        macDiv.removeClass('active');
-      }
-      console.log(mac.width());
+      console.log(scroll);
     });
   }
 }
