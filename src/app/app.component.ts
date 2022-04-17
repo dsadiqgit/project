f32c6431963031caa76437f97c64148b37a649f1
Dami Sadiq <dsadiq1994@gmail.com>
2022-04-17 04:29:21 +0100

    updating mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  onDeactivate() {
+    document.body.scrollTop = 0;
+  }
 }
--- src/app/portfolio/tutor-right/tutor-right.component.ts
+++ src/app/portfolio/tutor-right/tutor-right.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tutor-right',
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tutor-right.component.scss'],
 })
 export class TutorRightComponent implements OnInit {
-  constructor() {}
+  constructor(private window: Window) {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:scroll', []) onWindowScroll() {
+    var lgScreen = window.matchMedia('(min-width: 768px)');
+    var smScreen = window.matchMedia('(max-width: 767px)');
+
+    if (lgScreen.matches) {
+      var portfolioInfo = document.getElementById(
+        'portfolio-info'
+      ) as HTMLElement;
+      if (window.scrollY >= 240) {
+        portfolioInfo.classList.add('fixed');
+      }
+      if (window.scrollY < 230) {
+        portfolioInfo.classList.remove('fixed');
+      }
+    }
+
+    if (smScreen.matches) {
+      var portfolioInfo = document.getElementById(
+        'portfolio-info'
+      ) as HTMLElement;
+      portfolioInfo.classList.remove('fixed');
+    }
+  }
+
+  public test() {
+    console.log(window.scrollY);
+  }
 }
